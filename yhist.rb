5fccfa6ed7e7a3c11601fbb1d23aafe9290e0878
Murthy Gudipati <mgudipati@saventech.com>
2013-04-02 18:00:55 +0530

    taking sql dump

--- yhist.rb
+++ yhist.rb
@@ -17,6 +17,7 @@ $db.execute "CREATE TABLE IF NOT EXISTS MLPHist(
   volume    INTEGER,
   adjclose  FLOAT
   )"
+$db.execute "DELETE FROM MLPHist"
 
 $baseurl = "http://ichart.finance.yahoo.com/table.csv?s="
 
@@ -25,20 +26,24 @@ res = $db.execute "SELECT ticker FROM MLPMaster"
 res.each do |rec|
   puts rec
   # fetch the price history from yahoo finance...
-  CSV.new(open("#{$baseurl}#{rec[0]}"), :headers => :first_row, encoding: "UTF-8").each do |line|
-    puts line
-    $db.execute "INSERT INTO MLPHist 
-      VALUES 
-      (?,?,?,?,?,?,?,?)", 
-      [
-        rec[0],
-        Date.parse(line[0]).to_s,
-        line[1].to_f,
-        line[2].to_f,
-        line[3].to_f,
-        line[4].to_f,
-        line[5].to_i,
-        line[6].to_f
-      ]
+  begin
+    CSV.new(open("#{$baseurl}#{rec[0]}"), :headers => :first_row, encoding: "UTF-8").each do |line|
+      puts line
+      $db.execute "INSERT INTO MLPHist 
+        VALUES 
+        (?,?,?,?,?,?,?,?)", 
+        [
+          rec[0],
+          Date.parse(line[0]).to_s,
+          line[1].to_f,
+          line[2].to_f,
+          line[3].to_f,
+          line[4].to_f,
+          line[5].to_i,
+          line[6].to_f
+        ]
+    end
+  rescue OpenURI::HTTPError => e
+    puts e.message
   end
 end
