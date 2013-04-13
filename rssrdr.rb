c51e2cf2fa13e27d490ccee68311f443c8fa6240
Murthy Gudipati <mgudipati@saventech.com>
2013-04-13 10:49:27 +0530

    keyword search for rss feeds
    - added a method wordmatch to search for keywords in the title and
    description tags. only matching items are stored in the db.

--- rssrdr.rb
+++ rssrdr.rb
@@ -12,9 +12,25 @@ $db.execute "CREATE TABLE IF NOT EXISTS RSSFeed(
   link  VARCHAR NOT NULL,
   desc  VARCHAR NOT NULL
   )"
+$db.execute "DELETE FROM RSSFeed"
+
+# list of keywords...
+keyword_a = ['master limited partnership', 'mlp', 'pipeline', 'ngl', 'fracking', 'acmp']
 
 # list of feeds...
 url_a = ['http://phx.corporate-ir.net/corporate.rss?c=235260&Rule=Cat=news~subcat=ALL']
+
+# tries to find a match in the input on any of the words from the list
+def wordmatch (input, wordlist)
+  wordlist.each do |word|
+    if input =~ /#{word}/i
+      puts "Found keyword: #{word}"
+      return true
+    end
+  end
+  
+  return false
+end
 
 # loop through each feed...
 url_a.each do |url|
@@ -23,19 +39,21 @@ url_a.each do |url|
     open(url) do |rss|
       # parse the feed...
       feed = RSS::Parser.parse(rss)
-      puts "Title: #{feed.channel.title}"
+      puts "Channel Title: #{feed.channel.title}"
       # save the news items to the db...
       feed.items.each do |item|
         puts "Item: #{item.title}"
-        $db.execute "INSERT INTO RSSFeed 
-          VALUES 
-          (?,?,?,?)",
-          [
-            item.date.strftime("%m-%d-%Y"),
-            item.title,
-            item.link,
-            item.description
-          ]
+        if wordmatch(item.title, keyword_a) or wordmatch(item.description, keyword_a)
+          $db.execute "INSERT INTO RSSFeed 
+            VALUES 
+            (?,?,?,?)",
+            [
+              item.date.strftime("%m-%d-%Y"),
+              item.title,
+              item.link,
+              item.description
+            ]
+        end
       end
     end
   rescue OpenURI::HTTPError => e
